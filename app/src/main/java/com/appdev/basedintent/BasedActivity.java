215f715be4e7a542fb4834687c8731cfd6e639f6
goodnessidontknow <49700385+goodnessidontknow@users.noreply.github.com>
2021-03-10 09:20:49 -0800

    Did some stuff

--- app/src/main/java/com/appdev/basedintent/BasedActivity.java
+++ app/src/main/java/com/appdev/basedintent/BasedActivity.java
@@ -1,4 +1,32 @@
 package com.appdev.basedintent;
 
-public class BasedActivity {
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.View;
+
+public class BasedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_based);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new BasedFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
 }
