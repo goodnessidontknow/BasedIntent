c770ea683878084b16714439d4117472029a5a60
JustSomeSoggybread <evanhlee@outlook.com>
2021-03-21 00:37:21 -0700

    Finished chapter 8

--- app/src/main/java/com/appdev/basedintent/BasedActivity.java
+++ app/src/main/java/com/appdev/basedintent/BasedActivity.java
@@ -12,21 +12,10 @@ import androidx.fragment.app.FragmentManager;
 
 import android.view.View;
 
+
 public class BasedActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_based);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null) {
-            fragment = new BasedFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
+    protected Fragment createFragment() {
+        return new BasedFragment();
     }
 }
---
+++ app/src/main/java/com/appdev/basedintent/BasedListFragment.java
@@ -0,0 +1,91 @@
+package com.appdev.basedintent;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class BasedListFragment extends Fragment {
+
+    private RecyclerView mBasedRecyclerView;
+    private BasedAdapter mAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mBasedRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+
+        mBasedRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        updateUI();
+
+        return view;
+    }
+
+    private class BasedHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private Based mBased;
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+
+        public void bind(Based based) {
+            mBased = based;
+            mTitleTextView.setText(mBased.getTitle());
+            mDateTextView.setText(mBased.getDate().toString());
+        }
+        public BasedHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_based, parent, false));
+            itemView.setOnClickListener(this);
+        }
+        public void onClick(View view){
+            Toast.makeText(getActivity(), mBased.getTitle() + "clicked!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private class BasedAdapter extends RecyclerView.Adapter<BasedHolder> {
+        private List<Based> mBaseds;
+
+        public BasedAdapter(List<Based> crimes) {
+            mBaseds = crimes;
+        }
+
+        @NonNull
+        @Override
+        public BasedHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+
+            return new BasedHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull BasedHolder holder, int position) {
+            Based based = mBaseds.get(position);
+            holder.bind(based);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mBaseds.size();
+        }
+    }
+
+    private void updateUI(){
+        BasedLab basedLab = BasedLab.get(getActivity());
+        List<Based> baseds = basedLab.getCrimes();
+
+        mAdapter = new BasedAdapter((baseds));
+        mBasedRecyclerView.setAdapter(mAdapter);
+    }
+
+}
